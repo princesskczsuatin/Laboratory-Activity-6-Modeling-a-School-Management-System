7e727a8c0980320e6ded5e1db27ee20bb9591a5a
princesskczsuatin <princesskczsuatin@gmail.com>
2025-12-05 17:40:00 +0800

    SmartHomeMain.java

--- SmartHomeMain.java
+++ SmartHomeMain.java
@@ -5,22 +5,23 @@ public class SmartHomeMain {
         LampShade lamp2 = new LampShade(lamp1);
         Television tv = new Television();
         Microwave mw = new Microwave();
-
         Device[] devices = { ac, lamp1, lamp2, tv, mw };
-
-        System.out.println("\nTurning ON all devices:");
+            System.out.print("===================================");
+        System.out.println("\nTURNING ON ALL DEVICES:");
+                System.out.print("\n");
         Device.toggleDevices(devices, true);
-
+            System.out.print("===================================");
         System.out.println("\nNumber of powered devices: " +
                 Device.countPoweredOnDevices(devices));
-
+           System.out.print("===================================");
         System.out.println("\nAdjusting some device settings:");
-        ac.setTemperature(20);
-        lamp2.setLightColor("White");
-        tv.setVolume(30);
-        mw.setTimer(60);
-
-        System.out.println("\nTurning OFF all devices:");
+        ac.setTemperature(18);
+        lamp2.setLightColor("Pink");
+        tv.setVolume(27);
+        mw.setTimer(56);
+            System.out.print("===================================");
+        System.out.println("\nTURNING OFF ALL DEVICES:");
         Device.toggleDevices(devices, false);
+            System.out.print("===================================");
     }
 }
