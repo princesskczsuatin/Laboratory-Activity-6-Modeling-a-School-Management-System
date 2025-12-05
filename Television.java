07cc16f0af41b72c39ad35c6af1a30aca996f4fb
princesskczsuatin <princesskczsuatin@gmail.com>
2025-12-05 17:43:52 +0800

    Update Television.java

--- Television.java
+++ Television.java
@@ -1,29 +1,23 @@
 public class Television extends Device {
     private int channel;
     private int volume;
-
     public Television() {
         this.channel = 1;
         this.volume = 10;
     }
-
     public void setChannel(int channel) {
         this.channel = channel;
-        showStatus();
+        if (isOn()) showStatus();
     }
-
     public void setVolume(int volume) {
         this.volume = volume;
-        showStatus();
+        if (isOn()) showStatus();
     }
-
     @Override
     public void showStatus() {
         if (isOn()) {
-            System.out.println("Television ON - Channel: " + channel +
-                    ", Volume: " + volume + "%");
-        } else {
-            System.out.println("Television is OFF.");
+            System.out.println("TV  - Channel: " +
+                    channel + ", Volume: " + volume + "%");
         }
     }
 }
