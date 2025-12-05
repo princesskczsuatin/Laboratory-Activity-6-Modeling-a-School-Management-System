dc6ff1305d25bf936626779efa9bff9790e8f4f6
princesskczsuatin <princesskczsuatin@gmail.com>
2025-12-05 17:42:17 +0800

    Update AirConditioner.java

--- AirConditioner.java
+++ AirConditioner.java
@@ -1,29 +1,23 @@
 public class AirConditioner extends Device {
     private int fanSpeed;
     private int temperature;
-
     public AirConditioner() {
         this.fanSpeed = 3;
         this.temperature = 24;
     }
-
     public void setFanSpeed(int fanSpeed) {
         this.fanSpeed = fanSpeed;
-        showStatus();
+        if (isOn()) showStatus();
     }
-
     public void setTemperature(int temperature) {
         this.temperature = temperature;
-        showStatus();
+        if (isOn()) showStatus();
     }
-
     @Override
     public void showStatus() {
         if (isOn()) {
-            System.out.println("Air Conditioner ON - Fan Speed: " + fanSpeed +
+            System.out.println("AC - Fan Speed: " + fanSpeed +
                     ", Temperature: " + temperature + "°C");
-        } else {
-            System.out.println("Air Conditioner is OFF.");
         }
     }
 }
