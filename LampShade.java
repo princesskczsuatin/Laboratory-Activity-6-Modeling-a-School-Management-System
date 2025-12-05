1cce4dc99ec7b5210e9fedce8fdf63a2701c1433
princesskczsuatin <princesskczsuatin@gmail.com>
2025-12-05 17:43:09 +0800

    Update LampShade.java

--- LampShade.java
+++ LampShade.java
@@ -1,34 +1,27 @@
 public class LampShade extends Device {
     private int brightness;
     private String lightColor;
-
     public LampShade(int brightness, String lightColor) {
         this.brightness = brightness;
         this.lightColor = lightColor;
     }
-
     public LampShade(LampShade copy) {
         this.brightness = copy.brightness;
         this.lightColor = copy.lightColor;
     }
-
     public void setBrightness(int brightness) {
         this.brightness = brightness;
-        showStatus();
+        if (isOn()) showStatus();
     }
-
     public void setLightColor(String color) {
         this.lightColor = color;
-        showStatus();
+        if (isOn()) showStatus();
     }
-
     @Override
     public void showStatus() {
         if (isOn()) {
-            System.out.println("Lamp Shade ON - Brightness: " + brightness +
-                    "%, Light Color: " + lightColor);
-        } else {
-            System.out.println("Lamp Shade is OFF.");
+            System.out.println("Lampshade  - Brightness: " +
+                    brightness + "%, Light Color: " + lightColor);
         }
     }
 }
